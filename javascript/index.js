be8f4b5b975d84d66cc05ef5fbd3db9714a86ea3
Moataz <moataz_abdullah@outlook.com>
2025-07-05 08:10:29 +0300

    finished styling and game logic

--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,15 @@
-var rockChoice = document.querySelector(".rockChoice");
-var paperChoice = document.querySelector(".paperChoice");
-var scissorsChoice = document.querySelector(".scissorsChoice");
-
 var rockAudio = document.querySelector("#rockSound");
 var paperAudio = document.querySelector("#paperSound");
 var scissorsAudio = document.querySelector("#scissorSound");
+var playerScore = document.querySelector(".playerScore");
+var computerScore = document.querySelector(".computerScore");
+var choices = ["rock", "paper", "scissors"];
+var playerScoreCount = 0;
+var computerScoreCount = 0;
+var resultText = document.querySelector("#resultText");
+var allChoices = document.querySelectorAll(".choiceImg");
 
+/** SOUNDS SECTION */
 function stopAllSounds() {
   rockAudio.pause();
   paperAudio.pause();
@@ -21,25 +25,62 @@ function playRockMusic() {
   rockAudio.play();
 }
 
-rockChoice.addEventListener("click", function () {
-  stopAllSounds();
-  playRockMusic();
-});
-
 function playPaperMusic() {
   paperAudio.currentTime = 0;
   paperAudio.play();
 }
-paperChoice.addEventListener("click", function () {
-  stopAllSounds();
-  playPaperMusic();
-});
 
 function playScissorsMusic() {
   scissorsAudio.currentTime = 0;
   scissorsAudio.play();
 }
-scissorsChoice.addEventListener("click", function () {
-  stopAllSounds();
-  playScissorsMusic();
+allChoices.forEach((img) => {
+  img.addEventListener("click", function () {
+    const playerChoice = img.dataset.choice; // "rock", "paper" or "scissors"
+
+    // stop any playing sound
+    stopAllSounds();
+
+    // play the matching sound
+    if (playerChoice === "rock") playRockMusic();
+    else if (playerChoice === "paper") playPaperMusic();
+    else if (playerChoice === "scissors") playScissorsMusic();
+
+    // start the round with the right choice
+    startRound(playerChoice);
+  });
 });
+
+/** Game Logic Section */
+
+function getComputerChoice() {
+  let randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+}
+
+function startRound(playerChoice) {
+  var computerChoice = getComputerChoice();
+
+  var result;
+
+  if (playerChoice === computerChoice) {
+    result = "It's a draw";
+  } else if (
+    (playerChoice == "rock" && computerChoice == "scissors") ||
+    (playerChoice == "paper" && computerChoice == "rock") ||
+    (playerChoice == "scissors" && computerChoice == "paper")
+  ) {
+    result = "You Win!";
+    playerScoreCount++;
+  } else if (
+    (playerChoice == "rock" && computerChoice == "paper") ||
+    (playerChoice == "paper" && computerChoice == "scissors") ||
+    (playerChoice == "scissors" && computerChoice == "rock")
+  ) {
+    result = "You Lose!";
+    computerScoreCount++;
+  }
+  playerScore.textContent = playerScoreCount;
+  computerScore.textContent = computerScoreCount;
+  resultText.textContent = `You chose ${playerChoice}, Computer chose ${computerChoice}. ${result}`;
+}
